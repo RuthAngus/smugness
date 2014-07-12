f2db2d1a3a438c2cb528b0ac8ac69bbffae3d6d8
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-07-12 10:46:23 +0200

    buttons

--- smugness.py
+++ smugness.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-
 import forecastio
 import numpy as np
 from api_key import key
-
+from pygeocoder import Geocoder
+from farm import single_location, compare_locations, rank_locations, weather_index
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -18,7 +18,9 @@ api_key = key()
 # print lat, lng
 
 # Some locations:
-here = (51.2330, 0.2859)
+results = Geocoder.geocode("Tian'anmen, Beijing")
+here = results[0].coordinates
+here = (51.2330, -0.2859)
 sydney = (-33.86, -151.2094)
 new_york = (40.7127, -74.0059)
 tokyo = (35.6895, -139.6917)
@@ -60,9 +62,9 @@ def index():
     if loc1[0] > 0:
         compass = 'N'
     text1 = '%s %s %s' %(abs(loc1[0]), u'\N{DEGREE SIGN}', compass)
-    compass = 'E'
+    compass = 'W'
     if loc1[1] > 0:
-        compass = 'W'
+        compass = 'E'
     text2 = '%s%s %s' %(abs(loc1[1]), u'\N{DEGREE SIGN}', compass)
     temp1 = "%s%sC" %(int(temp1), u'\N{DEGREE SIGN}')
     temp2 = "%s%sC" %(int(temp2), u'\N{DEGREE SIGN}')
@@ -76,62 +78,64 @@ def index():
             img_name1=icon1, img_name2=icon2, ranks=ranks, text5=current2.summary, \
             description=description)
 
-# returns current forecast of one location
-def single_location(loc1):
-    lat1, lng1 = loc1
-    forecast1 = forecastio.load_forecast(api_key, lat1, lng1)
-    current1 = forecast1.currently()
-    return current1, current1.icon, current1.temperature
+# @app.route('/<lat><long>')
 
-# compares the weather index of two locations
-# if > 1, loc1 is better than loc2.
-def compare_locations(current1, loc2):
-    lat2, lng2 = loc2
-    forecast2 = forecastio.load_forecast(api_key, lat2, lng2)
-    current2 = forecast2.currently()
-    index1 = weather_index(current1)
-    index2 = weather_index(current2)
-    return current2, index1/index2, current2.icon, current2.temperature
-
-def rank_locations(latlongs, names):
-    # calculate current indices for all other locations
-    # change this so it updates every hour to save on api calls
-    indices = np.empty(len(names))
-    for i, loc in enumerate(latlongs):
-        forecast = forecastio.load_forecast(api_key, loc[0], loc[1])
-        current = forecast.currently()
-        indices[i] = weather_index(current)
-    # weight by goodness
-    # goodness could be 'learnt' or could take data from NOAO
-    # for now I'm going to make it up
-    #sydney, tokyo, riodj, new_york
-    goodness = [.8, .5, .9, .7]
-    goodness = [1., 1., 1., 1.]
-    likelihood = indices*goodness
-    tuples = zip(names, likelihood)
-    ranked = sorted(tuples, key=lambda x: x[1])
-    return ranked
-
-# order locations by longitude
-def order(otherlocs):
-    return
-
-# calculates a weather index for a location, based on its current forecast
-# currently uses temperature only
-def weather_index(current):
-    temp = current.temperature
-    feels_like = current.apparentTemperature
-    humidity = current.humidity*100
-    precip = current.precipIntensity
-    precipProb = current.precipProbability
-    windspeed = current.windSpeed
-    cloudcover = current.cloudCover
-    if current.precipIntensity != 0:
-        precipType = current.precipType
-    return temp
-
-def remove_space(phrase):
-    return phrase.replace(' ', '').replace('-','').lower()
+# # returns current forecast of one location
+# def single_location(loc1):
+#     lat1, lng1 = loc1
+#     forecast1 = forecastio.load_forecast(api_key, lat1, lng1)
+#     current1 = forecast1.currently()
+#     return current1, current1.icon, current1.temperature
+#
+# # compares the weather index of two locations
+# # if > 1, loc1 is better than loc2.
+# def compare_locations(current1, loc2):
+#     lat2, lng2 = loc2
+#     forecast2 = forecastio.load_forecast(api_key, lat2, lng2)
+#     current2 = forecast2.currently()
+#     index1 = weather_index(current1)
+#     index2 = weather_index(current2)
+#     return current2, index1/index2, current2.icon, current2.temperature
+#
+# def rank_locations(latlongs, names):
+#     # calculate current indices for all other locations
+#     # change this so it updates every hour to save on api calls
+#     indices = np.empty(len(names))
+#     for i, loc in enumerate(latlongs):
+#         forecast = forecastio.load_forecast(api_key, loc[0], loc[1])
+#         current = forecast.currently()
+#         indices[i] = weather_index(current)
+#     # weight by goodness
+#     # goodness could be 'learnt' or could take data from NOAO
+#     # for now I'm going to make it up
+#     #sydney, tokyo, riodj, new_york
+#     goodness = [.8, .5, .9, .7]
+#     goodness = [1., 1., 1., 1.]
+#     likelihood = indices*goodness
+#     tuples = zip(names, likelihood)
+#     ranked = sorted(tuples, key=lambda x: x[1])
+#     return ranked
+#
+# # order locations by longitude
+# def order(otherlocs):
+#     return
+#
+# # calculates a weather index for a location, based on its current forecast
+# # currently uses temperature only
+# def weather_index(current):
+#     temp = current.temperature
+#     feels_like = current.apparentTemperature
+#     humidity = current.humidity*100
+#     precip = current.precipIntensity
+#     precipProb = current.precipProbability
+#     windspeed = current.windSpeed
+#     cloudcover = current.cloudCover
+#     if current.precipIntensity != 0:
+#         precipType = current.precipType
+#     return temp
+#
+# def remove_space(phrase):
+#     return phrase.replace(' ', '').replace('-','').lower()
 
 if __name__ == '__main__':
     app.run(debug=True)
